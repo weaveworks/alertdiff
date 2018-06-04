f4d1e7e95f1c4ecd3d210062a1b21b2c7762e097
Adam Harrison <harrisonadamw@gmail.com>
2018-06-04 12:34:38 +0100

    Add `--excluded-labels` option to config

--- src/AlertDiff/Config.hs
+++ src/AlertDiff/Config.hs
@@ -11,12 +11,13 @@ import qualified Data.Set  as Set
 
 -- | Runtime configuration
 data Options =
-    Options { port :: Int
-            , expectedURL       :: Maybe String -- ^ AlertManager from which to pull expected alerts
-            , actualURL         :: Maybe String -- ^ AlertManager from which to pull actual alerts
-            , expectedTokenFile :: Maybe String -- ^ Authentication token for pulling expected alerts
-            , actualTokenFile   :: Maybe String -- ^ Authentication token for pulling actual alerts
-            , ignoreAlerts      :: [String]     -- ^ Name of alerts which should be excluded from comparison
+    Options { port              :: Int            -- ^ Port on which to serve API
+            , expectedURL       :: Maybe String   -- ^ AlertManager from which to pull expected alerts
+            , actualURL         :: Maybe String   -- ^ AlertManager from which to pull actual alerts
+            , expectedTokenFile :: Maybe String   -- ^ Authentication token for pulling expected alerts
+            , actualTokenFile   :: Maybe String   -- ^ Authentication token for pulling actual alerts
+            , ignoreAlerts      :: [String]       -- ^ Names of alerts which should be excluded from comparison
+            , excludedLabels    :: Set String     -- ^ Names of labels which should be excluded from comparison
             }
 
 stringList :: ReadM [String]
@@ -35,6 +36,7 @@ argParser = Options
     <*> optional (strOption (long "expected-token-file"))
     <*> optional (strOption (long "actual-token-file"))
     <*> option stringList (long "ignore-alerts" <> value [])
+    <*> option stringSet  (long "excluded-labels" <> value Set.empty)
 
 -- | Options parser yielding a runtime configuration from command line arguments
 optionParser :: ParserInfo Options
